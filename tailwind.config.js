ca9a08fc3d3e71717bfc0935b7917e0bb135291a
Lad3310 <joe.laird1@outlook.com>
2024-10-21 12:46:40 -0400

    Update eslint config, gitignore, and README

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'custom-purple': '#F3E8FF',
+        'custom-gray': '#F9FAFB',
       },
     },
   },
